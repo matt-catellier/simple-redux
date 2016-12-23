dee03fc2a4abf3fa1335459e185dcfaeb1b7f002
Matt Catellier <matt.catellier@adaptechsolutions.ca>
2016-12-23 13:33:33 -0800

    2 ways to load in reducer functions, see client.js

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,10 @@
 import { createStore  } from 'redux';
 import reducers from './reducers/';
 import middlewares from './middlewares/';
-import { fetchUsers } from './actions/userActions';
+import * as user from './actions/userActions';
 import { fetchTweets } from './actions/tweetActions';
 
 const store = createStore(reducers, middlewares);
 
-store.dispatch(fetchUsers()); // async
+store.dispatch(user.fetchUsers()); // async
 store.dispatch(fetchTweets()); // async
